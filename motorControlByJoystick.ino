26bb017d91d6ba4b1a8f81bba3ca9ee116130736
CYUZUZO010 <sandcyuzuzo@gmail.com>
2025-10-21 20:43:25 +0200

    Add files via upload

---
+++ motorControlByJoystick.ino
@@ -0,0 +1,28 @@
+#include <Servo.h>
+#define x_pin A0
+#define y_pin A1
+#define hardcoded_vcc1 A2
+#define hardcoded_gnd1 A3
+
+Servo myServo;
+
+void setup() {
+
+ Serial.begin(9600);
+ pinMode(hardcoded_vcc1,OUTPUT);
+ pinMode(hardcoded_gnd1, OUTPUT);
+ digitalWrite(hardcoded_vcc1, HIGH);
+ digitalWrite(hardcoded_gnd1, LOW);
+ myServo.attach(6);
+}
+void loop() {
+  int x_val=analogRead(x_pin);
+  int y_val=analogRead(y_pin);
+  int angle=map(x_val,0,1023,0,180);
+  myServo.write(angle);
+  delay(15);
+  Serial.print("X= ");
+  Serial.print(x_val);
+  Serial.print(", Y= ");
+  Serial.print(y_val);
+}
